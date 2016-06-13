780719086ae969f156642dccbf19f882c80bc7b1
Frank Kung <kfanjian@gmail.com>
2016-06-14 06:49:43 +0800

    Define options.

---
+++ src/redlot/options.go
@@ -0,0 +1,24 @@
+package redlot
+
+import (
+	"github.com/syndtr/goleveldb/leveldb/filter"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+)
+
+type Options struct {
+	DataPath          string
+	CacheSize         int
+	BlockSize         int
+	WriteBuffer       int
+	CompactionBackoff bool
+}
+
+func (c *Options) convert() *opt.Options {
+	return &opt.Options{
+		BlockCacheCapacity:       c.CacheSize * opt.MiB,
+		BlockSize:                c.BlockSize * opt.KiB,
+		DisableCompactionBackoff: !c.CompactionBackoff,
+		Filter:      filter.NewBloomFilter(10),
+		WriteBuffer: c.WriteBuffer * opt.MiB,
+	}
+}
