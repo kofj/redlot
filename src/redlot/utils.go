d2bca7f88d5e6ab9dc9698e66210acc123203128
Frank Kung <kfanjian@gmail.com>
2016-06-14 06:54:28 +0800

    Method that convert uint32 to bytes.

---
+++ src/redlot/utils.go
@@ -0,0 +1,9 @@
+package redlot
+
+import "encoding/binary"
+
+func Uint32ToBytes(v uint32) []byte {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, v)
+	return b
+}
---
+++ src/redlot/utils_test.go
@@ -0,0 +1,15 @@
+package redlot
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestUint32ToBytes(t *testing.T) {
+	expect := []byte{0x00, 0x00, 0x03, 0xe8}
+	b := Uint32ToBytes(1000)
+	if !bytes.Equal(expect, b) {
+		t.Logf("expect: % #X,but get: % #X", expect, b)
+		t.Fail()
+	}
+}
