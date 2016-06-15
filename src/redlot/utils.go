17f44fdf80166a6ce4a7849026d8450aac54f0cf
Frank Kung <kfanjian@gmail.com>
2016-06-15 20:49:04 +0800

    add util methods.

--- src/redlot/utils.go
+++ src/redlot/utils.go
@@ -1,9 +1,34 @@
 package redlot
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"strconv"
+)
 
 func uint32ToBytes(v uint32) []byte {
 	b := make([]byte, 4)
 	binary.BigEndian.PutUint32(b, v)
 	return b
+}
+
+func uint64ToBytes(v uint64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, v)
+	return b
+}
+
+func bytesToUint32(b []byte) uint32 {
+	return binary.BigEndian.Uint32(b)
+}
+
+func bytesToUint64(b []byte) uint64 {
+	return binary.BigEndian.Uint64(b)
+}
+
+func strToInt64(str string) int64 {
+	u, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		u = 0
+	}
+	return u
 }
