e4044cffd38c53733fe2c611e280d48d5468b578
Frank Kung <kfanjian@gmail.com>
2016-06-18 12:44:49 +0800

    Modify hash key struct.

--- redlot/t_hash.go
+++ redlot/t_hash.go
@@ -10,7 +10,6 @@ func encodeHashKey(name, key []byte) (buf []byte) {
 	buf = append(buf, typeHASH)
 	buf = append(buf, uint32ToBytes(uint32(len(name)))...)
 	buf = append(buf, name...)
-	buf = append(buf, uint32ToBytes(uint32(len(key)))...)
 	buf = append(buf, key...)
 	return
 }
@@ -18,7 +17,7 @@ func encodeHashKey(name, key []byte) (buf []byte) {
 func decodeHashKey(b []byte) (name, key []byte) {
 	nameLen := bytesToUint32(b[1:5])
 	name = b[5 : 5+nameLen]
-	key = b[9+nameLen:]
+	key = b[5+nameLen:]
 	return
 }
 
