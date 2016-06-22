d5926eaa884a40f73822dc958298e182039781da
Frank Kung <kfanjian@gmail.com>
2016-06-23 07:37:07 +0800

    Add test case for NewClient method.

--- client/client_test.go
+++ client/client_test.go
@@ -30,8 +30,21 @@ func TestMain(m *testing.M) {
 		time.Sleep(5e6)
 
 		r = m.Run()
+
+		client.Close()
 		os.RemoveAll("/tmp/data")
 		os.RemoveAll("/tmp/meta")
 		return r
 	}())
+}
+
+func TestNewClient(t *testing.T) {
+	o := &Options{
+		Addr: "127.0.0.1:9999",
+	}
+	client, err = NewClient(o)
+	if err != nil || client == nil {
+		t.Logf("client: %+v, err: %v\n", client, err)
+		t.Fail()
+	}
 }
