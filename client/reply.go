6ee5438d126e5be34da8d9cff7080738764c8058
Frank Kung <kfanjian@gmail.com>
2016-06-23 07:46:00 +0800

    Convert reply to bytes.

--- client/reply.go
+++ client/reply.go
@@ -11,3 +11,15 @@ type Reply struct {
 	State string
 	Data  [][]byte
 }
+
+func (r *Reply) bytes() []byte {
+	if len(r.Data) > 0 {
+		return r.Data[0]
+	}
+
+	return []byte{}
+}
+
+func (r *Reply) Bytes() []byte {
+	return r.bytes()
+}
