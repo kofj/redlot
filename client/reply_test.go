fd0ba3a3f5926f6ee31220ce15c2966cd2cf682a
Frank Kung <kfanjian@gmail.com>
2016-06-25 17:36:38 +0800

    Test case for Int method of reply.

--- client/reply_test.go
+++ client/reply_test.go
@@ -23,3 +23,20 @@ func TestString(t *testing.T) {
 	}
 
 }
+
+func TestInt(t *testing.T) {
+	r := client.Cmd("get", "k").Int()
+
+	if r != 0 {
+		t.Logf("expect int [0], but get[ %d]\n", r)
+		t.Fail()
+	}
+
+	client.Cmd("set", "int", "9223372036854775807")
+	r = client.Cmd("get", "int").Int()
+
+	if r != 9223372036854775807 {
+		t.Logf("expect int [9223372036854775807], but get[ %d]\n", r)
+		t.Fail()
+	}
+}
