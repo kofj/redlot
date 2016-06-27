805386b328300cef5776221556139d5a1cb93d75
Frank Kung <kfanjian@gmail.com>
2016-06-28 07:10:52 +0800

    Convert int,int8,int16,int32,int64 to send buf.

--- client/client.go
+++ client/client.go
@@ -135,6 +135,11 @@ func (c *Client) sendBuf(args []interface{}) (b []byte, err error) {
 			buf.WriteString(size)
 			buf.WriteString(s + "\r\n")
 			continue
+		case int, int8, int16, int32, int64:
+			s = fmt.Sprintf("%v", arg)
+			size = fmt.Sprintf("$%d\r\n", len(s))
+			buf.WriteString(size)
+			buf.WriteString(s + "\r\n")
 		}
 	}
 
